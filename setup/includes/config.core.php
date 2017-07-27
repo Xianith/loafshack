4c3629133e2e6000b9d55651d4ce4ca02b04ed7e
Xianith <xianith@gmail.com>
2017-07-27 18:58:26 +0000

    Double backup!

---
+++ setup/includes/config.core.php
@@ -0,0 +1,4 @@
+<?php
+if (!defined('MODX_CORE_PATH')) define ('MODX_CORE_PATH', '/srv/web/sites/loafshack.com/c0r3x/');
+if (!defined('MODX_CONFIG_KEY')) define('MODX_CONFIG_KEY', 'config');
+define ('MODX_SETUP_KEY', '@traditional@');
